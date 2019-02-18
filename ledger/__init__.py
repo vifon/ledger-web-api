b7218de3c118426b200fcb6ed16db04d4d36cd33
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-02-18 22:05:33 +0100

    Create the Entry class, add a complex input route

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -2,21 +2,30 @@
 
 from flask import Flask
 from flask_api import status
-import os
 
 import ledger.api
 
 app = Flask(__name__)
 
+
 @app.route("/amount/<amount>", methods=['POST'])
 def add_basic_payment(amount):
-    amount = amount.replace(",", ".").strip()
-    entry = ledger.api.create_entry(
+    return add_complex_payment(
         payee="AUTOMATIC",
         account_from="Liabilities:Karta kredytowa",
         account_to="Uncategorized",
         amount=amount,
     )
-    with open(os.environ['LEDGER_PATH'], 'a') as ledger_file:
-        print(entry, file=ledger_file)
-    return "", status.HTTP_201_CREATED
+
+
+@app.route("/account_from/<account_from>/account_to/<account_to>/payee/<payee>/amount/<amount>", methods=['POST'])
+def add_complex_payment(account_from, account_to, payee, amount):
+    amount = amount.replace(",", ".").strip()
+    entry = ledger.api.Entry(
+        payee=payee,
+        account_from=account_from,
+        account_to=account_to,
+        amount=amount,
+    )
+    entry.store()
+    return str(entry), status.HTTP_201_CREATED
--- ledger/api/__init__.py
+++ ledger/api/__init__.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python3
 
+import os
 import time
 
-template = """
+
+class Entry:
+    """A single Ledger entry.
+
+    >>> entry = Entry(
+    ...    payee="Burger King",
+    ...    account_from="Liabilities:Credit Card",
+    ...    account_to="Expenses:Food",
+    ...    amount="19.99 PLN",
+    ...    date="2019-02-15",
+    ... )
+
+    >>> print(entry)
+    <BLANKLINE>
+    2019-02-15 Burger King
+        Expenses:Food                              19.99 PLN
+        Liabilities:Credit Card
+    """
+
+    template = """
 {date} {payee}
-    {account_to:<34s}  {amount:>16}
+    {account_to:<34s}  {amount:>12.2f} PLN
     {account_from}
 """.rstrip()
 
-def create_entry(payee, account_from, account_to, amount):
-    date = time.strftime("%F")
-    return template.format(**locals())
+    def __init__(self, **kwargs):
+        self.payee = kwargs['payee']
+        self.account_from = kwargs['account_from']
+        self.account_to = kwargs['account_to']
+        self.amount = float(kwargs['amount'].split()[0])
+        self.date = kwargs.get('date', time.strftime("%F"))
+
+    def __str__(self):
+        return self.template.format(**vars(self))
+
+    def store(self, ledger_path=None):
+        if ledger_path is None:
+            ledger_path = os.environ['LEDGER_PATH']
+        with open(ledger_path, 'a') as ledger_file:
+            print(self, file=ledger_file)
+
+
+if __name__ == '__main__':
+    import doctest
+    import sys
+    sys.exit(doctest.testmod()[0])
